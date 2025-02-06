e1d86aa177096ba5970cb71e55511d89b9f9350f
Juan Veronez <juan.monteirov@gmail.com>
2025-02-06 15:56:31 -0300

    Update ORM table creation to use id incrementation

--- database.py
+++ database.py
@@ -32,7 +32,7 @@ Base = declarative_base()
 class Produto(Base):
     __tablename__ = "products"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, nullable=False)
     description = Column(String)
     price = Column(Float, nullable=False)
