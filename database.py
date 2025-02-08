6578e9bdb57fa55e26a5948da07b97b8834ed444
Juan Veronez <juan.monteirov@gmail.com>
2025-02-08 19:20:53 -0300

    update db to add product_copper table

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, DeclarativeBase
 
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='.env')  # Carrega as variáveis de ambiente do arquivo .env
-# load_dotenv(dotenv_path='.env.prod')  # Carrega as variáveis de ambiente do arquivo .env
+# load_dotenv(dotenv_path='.env.dev')  # Carrega as variáveis de ambiente do arquivo .env
+load_dotenv(dotenv_path='.env.prod')  # Carrega as variáveis de ambiente do arquivo .env
 
 db_user = os.getenv("POSTGRES_USER")
 db_password = os.getenv("POSTGRES_PASSWORD")
@@ -16,22 +16,31 @@ db_port = os.getenv("DB_PORT")
 # Configurando a conexão com o banco de dados
 # DATABASE_URL = "postgresql://meu_usuario:minha_senha@localhost:5432/meu_banco"
 
-DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode=require"
+print(DATABASE_URL)
 
 # Criando a engine de conexão
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={'sslmode': 'required'})
 
 # Criando a sessão
 
 Session = sessionmaker(bind=engine)
 
-Base = declarative_base()
+Base: DeclarativeBase = declarative_base()
 
 
 # Definindo o modelo de dados
 class Produto(Base):
     __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Float, nullable=False)
+    available = Column(Boolean, nullable=False)
+
+class ProdutoBronze(Base):
+    __tablename__ = "products_copper"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, nullable=False)
